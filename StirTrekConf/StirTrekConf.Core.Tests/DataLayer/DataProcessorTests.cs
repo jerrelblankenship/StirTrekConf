6f143a5bdc27abf1bdbd31afd0189c2473e351f6
Jerrel Blankenship <jerrel@jerrelblankenship.com>
2014-03-15 01:27:03 -0400

    Finished my first test on new project

--- StirTrekConf/StirTrekConf.Core.Tests/DataLayer/DataProcessorTests.cs
+++ StirTrekConf/StirTrekConf.Core.Tests/DataLayer/DataProcessorTests.cs
@@ -9,12 +9,25 @@
         [Test]
         public void DescerializrJsonFeed_Success()
         {
-            var jsonData = @"{""TimeSlots"":[{""Id"":8,""StartTime"":""8:00 AM"",""EndTime"":""9:00 AM""}],""Speakers"":[{""Id"":65,""Name"":""Jerrel  Blankenship"",""Bio"":""Jerrel is a software craftsman."",""ImageUrl"":""image1""},],""Sessions"":[{""Id"":74,""Name"":""Session Name"",""Abstract"":""Session Abstract"",""SpeakerIds"":[65],""TimeSlotId"":8,""TrackId"":30,""Tags"":[]}],""Sponsors"":[{""Id"":16,""Name"":""Sponsor Name"",""Description"":""Sponsor Description"",""LogoUrl"":""image2"",""URL"":""url Address"",""SponsorType"":""Inactive""}]}";
+            const string jsonData = @"{""TimeSlots"":[{""Id"":8,""StartTime"":""8:00 AM"",""EndTime"":""9:00 AM""}],
+                                ""Speakers"":[{""Id"":65,""Name"":""Peter Parker"",""Bio"":""Peter is a superhero."",""ImageUrl"":""image1""},],
+                                ""Sessions"":[{""Id"":74,""Name"":""Session Name"",""Abstract"":""Session Abstract"",""SpeakerIds"":[65],""TimeSlotId"":8,""TrackId"":30,""Tags"":[]}],
+                                ""Sponsors"":[{""Id"":16,""Name"":""Sponsor Name"",""Description"":""Sponsor Description"",""LogoUrl"":""image2"",""URL"":""url Address"",""SponsorType"":""Inactive""}]}";
+
             var processor = new DataProcessor();
 
             var result = processor.DescerializeJsonFeed(jsonData);
 
             Assert.That(result, Is.Not.Null);
+            Assert.That(result.TimeSlots.Count, Is.EqualTo(1));
+            Assert.That(result.Speakers.Count, Is.EqualTo(1));
+            Assert.That(result.Sessions.Count, Is.EqualTo(1));
+            Assert.That(result.Sponsors.Count, Is.EqualTo(1));
+
+            Assert.That(result.TimeSlots[0].Id, Is.EqualTo(8));
+            Assert.That(result.Speakers[0].Name, Is.EqualTo("Peter Parker"));
+            Assert.That(result.Sessions[0].Id, Is.EqualTo(74));
+            Assert.That(result.Sponsors[0].Description, Is.EqualTo("Sponsor Description"));
         }
     }
 }
